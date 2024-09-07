c399f2364e95a8c2a0389a911ae5401d3f699718
kurawa <151762850+galang01010@users.noreply.github.com>
2024-09-07 23:52:17 +0700

    Update sabdo.sh

--- sabdo.sh
+++ sabdo.sh
@@ -633,7 +633,7 @@ apt install rclone -y
 printf "q\n" | rclone config
 wget -O /root/.config/rclone/rclone.conf "${REPO}Cfg/rclone.conf"
 cd /bin
-git clone https://github.com/elvaretta-store/wondershaper.git
+git clone https://github.com/magnific0/wondershaper.git
 cd wondershaper
 sudo make install
 cd
